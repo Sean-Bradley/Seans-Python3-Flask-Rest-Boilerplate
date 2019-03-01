7014ecce0a48f4f983179e0fced211bc743df265
sean bradley <seanwasere@gmail.com>
2019-03-01 09:22:13 +0000

    added nose tests, updated readme

--- readme.md
+++ readme.md
@@ -25,3 +25,32 @@ curl -X POST http://127.0.0.1/request -H 'Content-Type: application/json' -d '{"
 curl -X DELETE http://127.0.0.1/request/04cfc704-acb2-40af-a8d3-4611fab54ada
 ```
 
+
+
+## Unit Test with Nose
+```bash
+nosetests --verbosity=2
+```
+**NOTE: the tests expect your are running the app via docker-compose -ip and on the same computer.**
+If you run the app from python directly,
+eg,
+```bash
+python app.py --debug
+```
+The endpoint will be http://localhost:5000 rather than the endpoint exposed via running it in the docker-compose. You should change the baseUrl in the script /tests/app_test.py to suit your environment
+
+### Test Output
+```bash
+$ nosetests --verbosity=2
+app_test.test_get_all_requests ... ok
+app_test.test_get_individual_request ... ok
+app_test.test_get_individual_request_404 ... ok
+app_test.test_add_new_record ... ok
+app_test.test_get_new_record ... ok
+app_test.test_delete_new_record ... ok
+
+----------------------------------------------------------------------
+Ran 6 tests in 0.113s
+
+OK
+```
---
+++ tests/app_test.py
@@ -0,0 +1,49 @@
+from nose.tools import assert_true
+import requests
+
+baseUrl = "http://127.0.0.1"
+UUID = None
+
+
+def test_get_all_requests():
+    global baseUrl
+    print(baseUrl)
+    response = requests.get('%s/request' % (baseUrl))
+    assert_true(response.ok)
+
+
+def test_get_individual_request():
+    global baseUrl
+    response = requests.get(
+        '%s/request/04cfc704-acb2-40af-a8d3-4611fab54ada' % (baseUrl))
+    assert_true(response.ok)
+
+
+def test_get_individual_request_404():
+    global baseUrl
+    response = requests.get('%s/request/an_incorrect_id' % (baseUrl))
+    assert_true(response.status_code == 404)
+
+
+def test_add_new_record():
+    global baseUrl
+    global UUID
+    payload = {'title': 'Good & Bad Book', 'email': 'testuser3@test.com'}
+    response = requests.post('%s/request' % (baseUrl), json=payload)
+    UUID = str(response.json()['request']['id'])
+    assert_true(response.ok)
+
+
+def test_get_new_record():
+    global baseUrl
+    global UUID
+    url = '%s/request/%s' % (baseUrl, UUID)
+    response = requests.get(url)
+    assert_true(response.ok)
+
+
+def test_delete_new_record():
+    global baseUrl
+    global UUID
+    response = requests.delete('%s/request/%s' % (baseUrl, UUID))
+    assert_true(response.ok)
