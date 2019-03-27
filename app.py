54984676304e329997bd42d980c97dcc49edd780
sean bradley <seanwasere@gmail.com>
2019-03-27 11:15:07 +0000

    pylinted app.py

--- app.py
+++ app.py
@@ -1,68 +1,69 @@
-import argparse
-from flask import Flask, jsonify, make_response, send_from_directory
-from flask_cors import CORS
-from routes import request_api
-import os
-from flask_swagger_ui import get_swaggerui_blueprint
+"""A Python Flask REST API BoilerPlate (CRUD) Style"""
 
-app = Flask(__name__)
+import argparse
+import os
+from flask import Flask, jsonify, make_response
+from flask_cors import CORS
+from flask_swagger_ui import get_swaggerui_blueprint
+from routes import request_api
+
+APP = Flask(__name__)
 
 ### swagger specific ###
 SWAGGER_URL = '/swagger'
 API_URL = '/static/swagger.json'
-swaggerui_blueprint = get_swaggerui_blueprint(
+SWAGGERUI_BLUEPRINT = get_swaggerui_blueprint(
     SWAGGER_URL,
     API_URL,
     config={
         'app_name': "Seans-Python-Flask-REST-Boilerplate"
     }
 )
-app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL)
-
-
-@app.route('/static/<path:path>')
-def send_static(path):
-    return send_from_directory('static', path)
+APP.register_blueprint(SWAGGERUI_BLUEPRINT, url_prefix=SWAGGER_URL)
 ### end swagger specific ###
 
 
-app.register_blueprint(request_api.get_blueprint())
+APP.register_blueprint(request_api.get_blueprint())
 
 
-@app.errorhandler(400)
-def handle400Error(error):
+@APP.errorhandler(400)
+def handle_400_error(_error):
+    """Return a http 400 error to client"""
     return make_response(jsonify({'error': 'Misunderstood'}), 400)
 
 
-@app.errorhandler(401)
-def handle401Error(error):
+@APP.errorhandler(401)
+def handle_401_error(_error):
+    """Return a http 401 error to client"""
     return make_response(jsonify({'error': 'Unauthorised'}), 401)
 
 
-@app.errorhandler(404)
-def handle404Error(error):
+@APP.errorhandler(404)
+def handle_404_error(_error):
+    """Return a http 404 error to client"""
     return make_response(jsonify({'error': 'Not found'}), 404)
 
 
-@app.errorhandler(500)
-def handle500Error(error):
+@APP.errorhandler(500)
+def handle_500_error(_error):
+    """Return a http 500 error to client"""
     return make_response(jsonify({'error': 'Server error'}), 500)
 
 
 if __name__ == '__main__':
 
-    parser = argparse.ArgumentParser(
+    PARSER = argparse.ArgumentParser(
         description="Seans-Python-Flask-REST-Boilerplate")
 
-    parser.add_argument('--debug', action='store_true',
+    PARSER.add_argument('--debug', action='store_true',
                         help="Use flask debug/dev mode with file change reloading")
-    args = parser.parse_args()
+    ARGS = PARSER.parse_args()
 
-    port = int(os.environ.get('PORT', 5000))
+    PORT = int(os.environ.get('PORT', 5000))
 
-    if args.debug:
+    if ARGS.debug:
         print("Running in debug mode")
-        cors = CORS(app)
-        app.run(host='0.0.0.0', port=port, debug=True)
+        CORS = CORS(APP)
+        APP.run(host='0.0.0.0', port=PORT, debug=True)
     else:
-        app.run(host='0.0.0.0', port=port, debug=False)
+        APP.run(host='0.0.0.0', port=PORT, debug=False)
